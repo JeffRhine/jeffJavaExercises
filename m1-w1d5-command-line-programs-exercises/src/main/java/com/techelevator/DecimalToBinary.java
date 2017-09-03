b3dda738034336c196a949b8974615798075cf4a
Jeff Rhine <j.s.rhine@gmail.com>
2017-09-03 15:51:35 -0400

    finished w1d5 exercises

--- m1-w1d5-command-line-programs-exercises/src/main/java/com/techelevator/DecimalToBinary.java
+++ m1-w1d5-command-line-programs-exercises/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
 Write a command line program which prompts the user for a series of decimal integer values  
 and displays each decimal value as itself and its binary equivalent
@@ -17,7 +19,38 @@ Please enter in a series of decimal values (separated by spaces): 460 8218 1 313
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
 
+		
+		System.out.print("Please enter in a series of decimal values (separated by spaces): ");
+		String userInput = input.nextLine();      
+		String [] decimalValues = userInput.split(" ");		// 
+		
+		
+		
+		int [] numberArray = new int [decimalValues.length];	
+		
+		
+		for (int i=0; i < decimalValues.length; i++){
+		numberArray[i] = Integer.parseInt(decimalValues[i]);
+		int number = numberArray[i];
+		String binNum = "";
+		
+			while( number > 0) {
+				
+				binNum = (number % 2) + binNum;
+						number = number / 2;
+			
+			}
+			System.out.println(decimalValues[i] + " in binary is " + binNum);
+			}	
+		}
+	
+		
 	}
 
-}
+	
+
+
--- m1-w1d5-command-line-programs-exercises/src/main/java/com/techelevator/Fibonacci.java
+++ m1-w1d5-command-line-programs-exercises/src/main/java/com/techelevator/Fibonacci.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  The Fibonacci numbers are the integers in the following sequence:  
 	0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
@@ -17,7 +19,42 @@ Please enter the Fibonacci number: 25
 public class Fibonacci {
 
 	public static void main(String[] args) {
-
+	     Scanner input = new Scanner(System.in);
+	        
+	        System.out.print("Please enter a number: ");
+	        int num = input.nextInt();
+	        input.nextLine();
+	       
+	        
+	 
+			 int[] fib = new int[num];
+	 fib[0] = 0;
+	 fib[1] = 1;
+	 for(int i =2; i < num; i++){
+		 fib[i] = fib[i-1] + fib[i-2];
+	 }
+	 for (int i=0; i<num; i++){
+		 System.out.print(fib[i] + " ");
+	 }
 	}
 
 }
+
+
+/*   for (int i = 1; i <= number; i++) {
+log(fibonacciRecusion(i) + " ");
+}
+
+public static int fibonacciRecusion(int number) {
+if (number == 1 || number == 2) {
+	return 1;
+}
+
+return fibonacciRecusion(number - 1) + fibonacciRecusion(number - 2); 
+}
+}private static void log(String number) {
+System.out.println(number);
+
+*/
+
+
