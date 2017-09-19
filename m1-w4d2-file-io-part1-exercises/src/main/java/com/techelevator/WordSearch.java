c84b3dfd020df93f11a804dc592368e3e961eba6
Jeff Rhine <j.s.rhine@gmail.com>
2017-09-19 15:08:25 -0400

    finished word search

--- m1-w4d2-file-io-part1-exercises/src/main/java/com/techelevator/WordSearch.java
+++ m1-w4d2-file-io-part1-exercises/src/main/java/com/techelevator/WordSearch.java
@@ -1,10 +1,96 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class WordSearch {
 
-	public static void main(String[] args) {
-	
-
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please type in a word:");
+		String userInput=input.nextLine();
+		System.out.println("Is this entry case sensitive? (Y)es or (N)o?");
+		String userInput2=input.nextLine();
+		File inputFile = new File("/Users/jeffrhine/workspace/exercises/m1-w4d2-file-io-part1-exercises/alices_adventures_in_wonderland.txt");
+		System.out.println(inputFile.exists());
+		
+		if(userInput2.equalsIgnoreCase("y")){
+		int lineCount=0;
+		try(Scanner input2 = new Scanner(inputFile)){
+			while(input2.hasNextLine()){
+				String line = input2.nextLine();
+				 lineCount++;
+				if(line.contains(userInput)){
+					
+					System.out.println(lineCount+" "+line);
+				}
+			}
+		}
+		}else if(userInput2.equalsIgnoreCase("n")){
+			int lineCount=0;
+			try(Scanner input2 = new Scanner(inputFile)){
+				while(input2.hasNextLine()){
+					String line = input2.nextLine();
+					 lineCount++;
+					if(line.toLowerCase().contains(userInput.toLowerCase())){
+						
+						System.out.println(lineCount+" "+line);
+					}
+		}
+			
+//			public static void main(String[] args) throws FileNotFoundException {
+//				Scanner input = new Scanner(System.in);
+//				System.out.println("Please type in a word:");
+//				String userInput=input.nextLine();
+//			
+//				File inputFile = new File("/Users/jeffrhine/workspace/exercises/m1-w4d2-file-io-part1-exercises/alices_adventures_in_wonderland.txt");
+//				System.out.println(inputFile.exists());
+//				int lineCount=0;
+//				try(Scanner input2 = new Scanner(inputFile)){
+//					while(input2.hasNextLine()){
+//						String line = input2.nextLine();
+//						 lineCount++;
+//						if(line.contains(userInput)){
+//							
+//							System.out.println(lineCount+" "+line);
+//						}
+//					}
+//			
+			
+			
+			
+			
+			
+			
+			
+			//if(! line.equals(" "){
+//		
+//		try(Scanner input = new Scanner(inputFile)) {
+//			int answer = 0;
+//			while(input.hasNextLine()) {
+//				String line = input.nextLine();
+//				String [] parts = line.split("[\\!\\.\\?]");
+//					answer += parts.length - 1;
+//					}
+//					System.out.println(answer);
+//				} catch(Exception e) {
+//					System.out.println(e.getMessage());
+//				}
+//		
+//		try(Scanner inputW = new Scanner(inputFile)) {
+//			int answer = 0;
+//			while(inputW.hasNextLine()) {
+//				String line = inputW.nextLine();
+//				String [] parts = line.split("[\\s\\d\\W]+");
+//					answer += parts.length - 1;
+//					}
+//					System.out.println(answer);
+//				} catch(Exception e) {
+//					System.out.println(e.getMessage());
+//				}
+//	
 	}
-
+}
+}
 }
---
+++ m1-w4d2-file-io-part1-exercises/src/test/java/WordSearchTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.techelevator.WordSearch;
+
+public class WordSearchTest {
+	WordSearch sut;
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAlice() {
+		;
+	}
+
+}
