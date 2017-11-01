c673be36fb759916ca4b42f8c1ac913e69eaf71f
Jeff Rhine <j.s.rhine@gmail.com>
2017-11-01 15:47:49 -0400

    day three

--- m4-w10d2-java-history_geek_exercises/src/main/webapp/js/exercises/event-exerises.js
+++ m4-w10d2-java-history_geek_exercises/src/main/webapp/js/exercises/event-exerises.js
@@ -1,5 +1,19 @@
 ﻿/// <reference path="../jquery-3.1.1.js" />
 
 $(document).ready(function () {
+$("#SameShipping").on('click',function(){
+	$('#ShippingAddress1').val($("#BillingAddress1").val());
+	$('#ShippingAddress2').val($("#BillingAddress2").val());
+	$('#ShippingCity').val($("#BillingCity").val());
+	$('#ShippingState').val($("#BillingState").val());
+	$('#ShippingPostalCode').val($("#BillingPostalCode").val());
+});
+$("input[name='ShippingType']").on('click',function(){
+	var checkbox= $(this);
+	var checkShipping = checkbox.attr('data-cost');
+	$('#shipping > .price').text('$'+checkShipping);
+var sbtotal = $('#subtotal > .price').text().substring(1);
+$('#grandtotal > .price').text('$'+parseFloat(checkShipping)+parseFloat(sbtotal));	
+});
 
 });
